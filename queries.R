2b74ed84460a35155eb816417935e679ab81abb8
Eric-Gomez-Hernandez <egomez_99@yahoo.com>
2021-07-07 10:42:12 +0000

    Terminado el projecto

--- queries.R
+++ queries.R
@@ -12,8 +12,12 @@
 
 #install.packages("DBI")
 #install.packages("RMySQL")
+#install.packages("dplyr")
+#install.packages("ggplot2")
 library(DBI)
 library(RMySQL)
+library(dplyr)
+library(ggplot2)
 
 MyDataBase <- dbConnect(
   drv = RMySQL::MySQL(),
@@ -25,11 +29,33 @@ MyDataBase <- dbConnect(
 # 4. Una vez hecha la conexión a la BDD, generar una búsqueda con dplyr
 # que devuelva el porcentaje de personas que hablan español en todos los países
 
+# Primero vemos las tablas que contiene esta BDD
+dbListTables(MyDataBase)
+# De la tabla "CountryLanguaje"atenderemos nuestra solicitud.
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+
+str(DataDB)
+head(DataDB)
+
+porcSpanish <- DataDB %>% filter(Language == "Spanish")
+str(porcSpanish)  # Observamos que ya es un dataFrame
+names(porcSpanish) # Aqui solo vemos los nombres de los campos.
+head(porcSpanish)
+
+
 # 5. Realizar una gráfica con ggplot que represente este porcentaje de tal 
 # modo que en el eje de las Y aparezca el país y en X el porcentaje, 
 # y que diferencíe entre aquellos que es su lengua oficial y los que no, 
 # con diferente color (puedes utilizar geom_bin2d() ó geom_bar() y coord_flip(),
 # si es necesario para visualizar mejor tus gráficas)
 
+porcSpanish %>% ggplot(aes( x = CountryCode, y=Percentage, fill = IsOfficial)) + 
+  geom_bar(stat = "identity") +coord_flip()
+
+porcSpanish %>% ggplot(aes( x = CountryCode, y=Percentage, fill = IsOfficial )) + 
+  geom_bin2d() +
+  coord_flip()
+
 # 6. Una vez hecho esto hacer el commit y push para mandar tu archivo 
 # (queries.R), al repositorio de Github Reto_Sesion_7
+
